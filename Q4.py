f7d54e3ee58ce452161f7192236ec2a316994f91
Aaaddiiiii <amjhapcm@gmail.com>
2026-05-04 22:31:47 +0530

    brute-force / straightforward approach of - 4. Median of Two Sorted Arrays

---
+++ Q4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1.extend(nums2)
+        nums1 = sorted(nums1)
+
+        n = len(nums1)
+
+        if n == 0:
+            return 0
+        elif n % 2 == 1:
+            return nums1[n // 2]
+        else:
+            return (nums1[n//2 - 1] + nums1[n//2]) / 2
