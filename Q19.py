26a854f5220545d16ee2f634f1476025f1cb4c17
Aaaddiiiii <amjhapcm@gmail.com>
2026-05-11 21:13:01 +0530

    19. Remove Nth Node From End of List

--- Q19.py
+++ Q19.py
@@ -4,23 +4,18 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
-        dummy = ListNode()  
-        tail = dummy 
-        while list1 and list2:
-            if list1.val <= list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
-            tail = tail.next 
-        if list1:
-            tail.next = list1
-        else:
-            tail.next = list2
-        return dummy.next 
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        temp = head
+        count = 0
+        while temp:
+            count = count + 1
+            temp = temp.next
+        temp = head
+        if count - n == 0:
+            temp = temp.next
+            return temp 
+        while count - n > 1:
+            temp = temp.next
+            count = count - 1
+        temp.next = temp.next.next
+        return head
