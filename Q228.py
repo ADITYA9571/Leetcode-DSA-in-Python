d067c1a372148ea8e2e45002729c7316d672efc3
Aaaddiiiii <amjhapcm@gmail.com>
2026-05-15 09:19:04 +0530

    228. Summary Ranges

--- Q228.py
+++ Q228.py
@@ -3,19 +3,15 @@ class Solution:
         list1 = []
         temp1 = 0
         temp2 = 0
-        length = len(nums)
         flag = 0
-        
         for i, num in enumerate(nums):
             if flag == 0:
                 temp1 = num
                 flag = 1
             if num != nums[-1] and num == nums[i+1] - 1:
-                later = 1
                 if num != nums[-2]:
                     continue 
             else:
-                later = 0
                 temp2 = num
                 flag = 0
                 if temp1 == temp2:
