e888aca3a2177830b695ec9598f45a4a3ab42a18
Aaaddiiiii <amjhapcm@gmail.com>
2026-05-30 16:40:08 +0530

    56. Merge Intervals

---
+++ Q56.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        i = 1
+        intervals.sort()
+        while i < len(intervals):
+            if intervals[i-1][-1] >= intervals[i][0]:
+                if intervals[i][-1] <= intervals[i-1][-1]:
+                    del intervals[i]
+                    continue
+                intervals[i-1][-1] = intervals[i][-1]
+                del intervals[i]
+            else:
+                i += 1
+        return intervals 
+
