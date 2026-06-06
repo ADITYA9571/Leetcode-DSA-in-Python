107cc6f3768bb6cbd88020f4986c32cebd17d43c
Aaaddiiiii <amjhapcm@gmail.com>
2026-06-06 08:42:48 +0530

    2574. Left and Right Sum Differences

---
+++ Q2574.py
@@ -0,0 +1,16 @@
+# PART OF DAILY LEETCODE PROBLEM
+class Solution:
+    def leftRightDifference(self, nums: List[int]) -> List[int]:
+        if len(nums) == 1:
+            return [0]
+        left = [0]
+        right = [sum(nums)]
+        for i, num in enumerate(nums):
+            left.append(left[-1]+ num)
+            right.append(right[-1]-num)
+        del left[-1]
+        del right[0]
+        ans = []
+        for i in range(len(left)):
+            ans.append(abs(left[i] - right[i]))
+        return ans 
